0a602ce6a11aced1716b65e563c2f25ed3dc30f3
Leonardo Ramos dos Santos <leonardoramosdossantos92@gmail.com>
2024-06-11 03:06:11 -0300

    Aula08

--- CursoJava/Aula08/src/cursoJava.java
+++ CursoJava/Aula08/src/cursoJava.java
@@ -6,14 +6,18 @@ public class cursoJava {
         final int linha = 3;
         final int coluna = 5;
 
+        int nu[][] = {
+        {0,1,2,3,4},
+        {10,11,12,13,14},
+        {100,101,102,103,104}};
+
         int numero[][] = new int[linha][coluna];
         for (int i = 0; i < linha; i++) {
-            for (int j = 0; j < coluna; j++) {
-                
+            for (int j = 0; j < coluna; j++) {                
                 numero[i][j] = new SecureRandom().nextInt(100);
             }
         }
-//
+        //
         //for (int i = 0; i < linha; i++) {
         //    for (int j = 0; j < coluna; j++) {
         //        System.out.printf("%2d |",numero[i][j]);                
@@ -21,13 +25,11 @@ public class cursoJava {
         //    System.out.println("");
         //}
 
-        for (int num[] : numero) {
+        for (int num[] : nu) {
             for (int v : num) {
-                System.out.printf("| %2d |", v);
-                System.out.println();
-                System.out.printf("------");
-                System.out.println();
+                System.out.printf("%3d |", v);       
             }
+            System.out.println();
         }
     }
 }
