d161da31cb583fc5469ae8c81911e8a6be96d49f
Leonardo Ramos dos Santos <leonardoramosdossantos92@gmail.com>
2024-06-11 02:28:23 -0300

    Initial commit

---
+++ CursoJava/Aula08/src/cursoJava.java
@@ -0,0 +1,28 @@
+import java.security.SecureRandom;
+
+public class cursoJava {
+
+    public static void main(String[] args) {
+        final int linha = 3;
+        final int coluna = 5;
+
+        //int numero[][] = new int[linha][coluna];
+        //for (int i = 0; i < linha; i++) {
+        //    for (int j = 0; j < coluna; j++) {
+        //        
+        //        numero[i][j] = new SecureRandom().nextInt(100);
+        //    }
+        //}
+//
+        //for (int i = 0; i < linha; i++) {
+        //    for (int j = 0; j < coluna; j++) {
+        //        System.out.printf("%2d |",numero[i][j]);                
+        //    }
+        //    System.out.println("");
+        //}
+
+        for (int string : args) {
+            
+        }
+    }
+}
