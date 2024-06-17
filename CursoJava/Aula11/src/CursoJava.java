556af826ea291b601bc29aa4db510ea94d2bf374
Leonardo Ramos dos Santos <leonardoramosdossantos92@gmail.com>
2024-06-17 17:45:36 -0300

    Aula12.java

--- CursoJava/Aula11/src/CursoJava.java
+++ CursoJava/Aula11/src/CursoJava.java
@@ -1,9 +1,19 @@
 public class CursoJava {
     public static void main(String[] args) {
-        int num = 1;
+        int num = 0;
         Jogador j1 = new Jogador(num++);
         Jogador j2 = new Jogador(num++);     
-        Jogador j4 = new Jogador(num++);     
-        Jogador j5 = new Jogador(num++);        
+        Jogador j3 = new Jogador(num++);     
+        Jogador j4 = new Jogador(num++);       
+        
+        j1.addVidas();
+        j1.addVidas();
+        System.out.printf("%n Vidas do jogador 1:%d", j1.getVidas());
+
+        j2.addVidas();
+        System.out.printf("%n Vidas do jogador 2:%d", j2.getVidas());
+        System.out.printf("%n Vidas do jogador 3:%d", j3.getVidas());
+        System.out.println();
+            
     }
 }
--- CursoJava/Aula11/src/Jogador.java
+++ CursoJava/Aula11/src/Jogador.java
@@ -1,8 +1,24 @@
 public class Jogador {
     private int num = 0;
+    private int vidas = 0;
+    private final int maxValue = 3;
 
     public Jogador(int num){
         this.num=num;
+        this.vidas = 1;
         System.out.printf("Jogador numero %d criador%n", num);
+    }
+
+    public int getVidas(){
+        return this.vidas;  
+    }    
+
+    public void addVidas(){       
+        if (this.vidas < maxValue) {
+            this.vidas++;
+        }else {
+            System.out.println("Valor de vidas, cheias!");
+            System.out.println();
+        }
     }
 }
