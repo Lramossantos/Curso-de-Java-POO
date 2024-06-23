9e7f1983d053f628b764c31e56d995d95c59444f
Leonardo Ramos dos Santos <leonardoramosdossantos92@gmail.com>
2024-06-23 00:20:54 -0300

    Aula18.java

---
+++ CursoJava/Aula18/src/Animal.java
@@ -0,0 +1,28 @@
+public class Animal implements SerVivo{    
+
+    public Animal(){
+
+    }
+    
+    @Override
+    public void mover() {
+        
+        
+    }
+
+    @Override
+    public void comer(int massa) {
+        
+        
+    }
+
+    @Override
+    public void info() {
+        System.out.printf("Vivo: %s%n",this.vivo?"Sim":"Nao");
+        System.out.printf("");
+        System.out.printf("");
+        System.out.printf("");
+        
+    }
+
+}
---
+++ CursoJava/Aula18/src/CursoJava.java
@@ -0,0 +1,10 @@
+public class CursoJava {    
+    public static void main(String[] args) throws Exception {
+        Animal n[] = new Animal[2];
+        n[0] = new Animal();
+        n[0].info();
+        
+
+
+    }
+}
