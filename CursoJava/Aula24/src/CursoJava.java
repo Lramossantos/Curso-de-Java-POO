08e3ac13ffb24e5bd996d1fbb4a9f54002113710
Leonardo Ramos dos Santos <leonardoramosdossantos92@gmail.com>
2024-06-28 14:45:51 -0300

    Como criar e manipular Array em Java [Aprenda nesta aula] - Curso de Java - Aula 24

---
+++ CursoJava/Aula24/src/CursoJava.java
@@ -0,0 +1,19 @@
+//Como criar e manipular Array em Java [Aprenda nesta aula] - Curso de Java - Aula 24
+public class CursoJava {
+    public static void main(String[] args) throws Exception {
+        int num[] = new int[]{1,2,3,4,5,6,7,8,9,10};
+      /*num[0]=52;
+        num[1]=100;
+        num[2]=3;
+        num[3]=15;
+        num[4]=527;
+        num[5]=112;
+        num[6]=109;  */
+
+        for (int i : num) {
+            System.out.println(i);            
+        }
+        
+        
+    }
+}
